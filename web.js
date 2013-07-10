723fbf290c14dec8df47ba3b776a7e1628e202d2
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2013-07-10 06:52:52 +0000

    Made web.js read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
+// Buffer class is a global; don't need to require it
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indexString = fs.readFileSync('index.html').toString();
+  response.send(indexString);
 });
 
 var port = process.env.PORT || 5000;
